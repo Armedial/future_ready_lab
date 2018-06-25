78b2896ec29580979547b66562f0dec66cb991d2
Armedial <kenneth.lam0808@gmail.com>
2018-06-25 14:03:31 -0700

    ,

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -1,1 +1,32 @@
 /*eslint-env browser*/
+
+
+function playGame(){
+
+
+
+
+alert("First enter a low number, then a high number. Then guess a random number between the two.");
+
+//get the low and high bounds
+//uses parseInt() to make sure we have numbers and not strings
+var from = parseInt(prompt("Enter the lower bound"));
+
+var to = parseInt(prompt("Enter the higher bound"));
+
+var target = Math.round(Math.random() * (to-from) + from);
+
+var currentGuess = parseInt(prompt("Guess a Number between" + from + " and " +to));
+
+var totalGuesses =1;
+
+if (currentGuess < target){
+    currentGuess= parseInt( prompt("Enter a higher number") ) totalGuesses++;
+}
+//loop until user guesses correct number
+while(currentGuess !=target){}
+else if(currentGuess>target){currentGuess=parseInt(prompt("Enter a lower number")); totalGuesses++}
+
+else {alert("it took" +totalGuesses + "Tries to guess this number")}
+
+}
