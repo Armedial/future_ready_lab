8d1ec8e99fd1cfe588504a89f3c5ca35357e60a0
Armedial <kenneth.lam0808@gmail.com>
2018-07-02 14:01:57 -0700

    Sketch

--- Week_2/Processing JS/sketch.js
+++ Week_2/Processing JS/sketch.js
@@ -11,7 +11,7 @@ var circle = {
 
 function setup() {
   createCanvas(640, 480);
-    background(20,55,76);
+    background(20,55,76); //background color
     frameRate(500);
 }
 
@@ -19,14 +19,14 @@ function draw() {
   fill(circle.color);
     ellipse(circle.xCoor, circle.yCoor, circle.diameter);
     
-    if(circle.xCoor > 640 || circle.xCoor < 0){
-        circle.color = [random(0,255),random(0,255),random(0,255)]
-        circle.xSpeed = -circle.xSpeed;
-        circle.diameter = random(20,80);
-    } else if(circle.yCoor > 480 || circle.yCoor < 0){
-        circle.color = [random(0,255),random(0,255),random(0,255)];
-        circle.ySpeed = -circle.ySpeed;
-        circle.diameter = random(20,80);
+    if(circle.xCoor > 640 || circle.xCoor < 0){                      //if x goes out of the canvas
+        circle.color = [random(0,255),random(0,255),random(0,255)]   //change circle color
+        circle.xSpeed = -circle.xSpeed;                              //change circle direction
+        circle.diameter = random(20,80);                             //change circle size
+    } else if(circle.yCoor > 480 || circle.yCoor < 0){               //if y goes out of canvas
+        circle.color = [random(0,255),random(0,255),random(0,255)];  //change circle color
+        circle.ySpeed = -circle.ySpeed;                              //change circle direction
+        circle.diameter = random(20,80);                             //change circle size
         
     } else {
         circle.xSpeed = circle.xSpeed;
