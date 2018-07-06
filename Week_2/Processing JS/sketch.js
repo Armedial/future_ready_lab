ea47c76e07355545711bea04b7b5a158b7be9d3c
Armedial <kenneth.lam0808@gmail.com>
2018-07-06 13:17:03 -0700

    Made changes so that the amount of  circles are random without mathrandom

--- Week_2/Processing JS/sketch.js
+++ Week_2/Processing JS/sketch.js
@@ -1,38 +1,64 @@
 
-//setup is run once at the beginning before we draw
-var circle = {
-    diameter: 80,
-    xCoor: 0,
-    yCoor: 0,
-    color: [255,0,0], //Red Circle
-    xSpeed: 5,
-    ySpeed: 5
-}
-
-function setup() {
-  createCanvas(640, 480);
-    background(20,55,76); //background color
-    frameRate(500);
-}
-
-function draw() {
-  fill(circle.color);
-    ellipse(circle.xCoor, circle.yCoor, circle.diameter);
-    
-    if(circle.xCoor > 640 || circle.xCoor < 0){                      //if x goes out of the canvas
-        circle.color = [random(0,255),random(0,255),random(0,255)]   //change circle color
-        circle.xSpeed = -circle.xSpeed;                              //change circle direction
-        circle.diameter = random(20,80);                             //change circle size
-    } else if(circle.yCoor > 480 || circle.yCoor < 0){               //if y goes out of canvas
-        circle.color = [random(0,255),random(0,255),random(0,255)];  //change circle color
-        circle.ySpeed = -circle.ySpeed;                              //change circle direction
-        circle.diameter = random(20,80);                             //change circle size
-        
-    } else {
-        circle.xSpeed = circle.xSpeed;
-        circle.ySpeed = circle.ySpeed;
+    //setup is run once at the beginning before we draw
+    /*var circle = {
+        diameter: 80,
+        xCoor: 0,
+        yCoor: 0,
+        color: [255,0,0], //Red Circle
+        xSpeed: 5,
+        ySpeed: 5
     }
-    
-    circle.xCoor += circle.xSpeed;
-    circle.yCoor += circle.ySpeed;
+    */
+   function setup() {
+    canvas = createCanvas(prompt("Enter width of canvas: "),prompt("Enter height of canvas: "));
+    frameRate(15);
+    makeCircle();
+}
+//random color
+function randomColor(){
+    return [random(0,255), random(0,255), random(0,255)];
+}
+//circle variables
+function Circle(x, y, size, color, xSpeed, ySpeed){
+    this.xCoor = x;
+    this.yCoor = y;
+    this.diameter = size;
+    this.color = color; //black default
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+}
+var circleList = [];
+
+
+function makeCircle(){
+    var balls = random([2,5,9,16]);
+    for (var b = 0; b < balls; b++){ //make circles
+        circleList.push(new Circle(random(width),random(height),50, [random(255),random(255),random(255)],random(10,30),random(10,30)));
+    }
+}
+
+function draw(){
+    background(20,55,76);
+    for (var i = 0; i < circleList.length; i++){// make the canvas and circles
+        fill(circleList[i].color);
+        ellipse(circleList[i].xCoor, circleList[i].yCoor, circleList[i].diameter);
+        for(var x = 0; x < circleList.length; x++){
+            var dx = circleList[i].xCoor - circleList[x].xCoor;
+            var dy = circleList[i].yCoor - circleList[x].yCoor;
+          
+        }
+        
+        if (circleList[i].xCoor > width || circleList[i].xCoor < 0){ //change circle properties if touch x border
+            circleList[i].color = randomColor(); 
+            circleList[i].xSpeed = -circleList[i].xSpeed; 
+            circleList[i].diameter = random(20,70); 
+        }
+        if (circleList[i].yCoor > height || circleList[i].yCoor < 0){ //change circle properties if touch y border
+            circleList[i].color = randomColor();
+            circleList[i].ySpeed = -circleList[i].ySpeed;
+            circleList[i].diameter = random(20,70);
+        }
+        circleList[i].xCoor += circleList[i].xSpeed;
+        circleList[i].yCoor += circleList[i].ySpeed;
+    }
 }
